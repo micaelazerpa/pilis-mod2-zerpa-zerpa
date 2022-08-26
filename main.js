7e821f95ac10742afcfc96a0f8479b275814e1bf
Micaela Zerpa <micazerpa14@gmail.com>
2022-08-26 12:01:51 -0300

    js

---
+++ main.js
@@ -0,0 +1,19 @@
+
+async function getIp() {
+  try {
+    let response = await fetch("https://api.ipify.org/?format=json");
+    let ipResponse = await response.json();
+    console.log(ipResponse);
+
+    let responseLocation = await fetch("http://ip-api.com/json/" + ipResponse.ip);
+    let locationResponse = await responseLocation.json();
+    console.log(locationResponse);
+
+    let responseEvento = await fetch("https://pro.openweathermap.org/data/2.5/forecast/hourly?lat=-24.18847&lon=-65.29989&appid=66316d0d6ee670997f4c51bfe416f342");
+    let locationEvento = await responseEvento.json();
+    console.log(locationEvento);
+  } catch {
+    console.log("Error al mostrar el clima");
+  }
+}
+getIp();
